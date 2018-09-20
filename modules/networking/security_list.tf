ac2fd5e32d4f5221419a9747dcaa113b0ce2b223
Jonathan Barcus <jonathan.barcus@oracle.com>
2018-09-20 15:51:35 -0400

    Aditya = 'the man'.  Added and ran SECURITY_LIST successfully

---
+++ modules/networking/security_list.tf
@@ -0,0 +1,46 @@
+#creating a security list
+resource "oci_core_security_list" "security_list" {
+ compartment_id = "${var.created_compartment_id}"
+ vcn_id         = "${oci_core_virtual_network.vcn.id}"
+ display_name   = "SecurityList"
+
+ // allow outbound tcp traffic on all ports
+ egress_security_rules {
+   destination = "0.0.0.0/0"
+   protocol    = "all"
+ }
+
+ // allow inbound ssh traffic from a specific port
+ ingress_security_rules {
+   protocol  = "6"         // tcp
+   source    = "0.0.0.0/0"
+   stateless = true
+
+   tcp_options {
+// these represent destination port range
+     "min" = 22
+     "max" = 22
+   }
+ }
+
+// allow inbound http traffic from a specific port
+ ingress_security_rules {
+   protocol  = "6"         // tcp
+   source    = "0.0.0.0/0"
+   stateless = true
+
+   tcp_options {
+// these represent destination port range
+     "min" = 80
+     "max" = 80
+   }
+ }
+
+
+ // allow inbound icmp traffic of a specific type
+ ingress_security_rules {
+   protocol  = "1"         // icmp
+   source    = "0.0.0.0/0"
+   stateless = true
+ }
+}
